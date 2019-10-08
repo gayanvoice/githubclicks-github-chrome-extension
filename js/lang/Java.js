cc686b84c17c7ab37296b0077c83747b7b990fa3
gayankuruppu <example@example.com>
2019-10-08 23:06:24 +0530

    Update Java.js

--- js/lang/Java.js
+++ js/lang/Java.js
@@ -2,6 +2,7 @@ class Java {
     constructor(url, variable) {
         this.url = url;
         this.variable = this.getVariable(variable);
+        this.filetype = '.java';
     }
 
     showVariable() {
@@ -10,25 +11,27 @@ class Java {
             this.goTop();
         } else if (this.isClass(this.variable)){
             this.goClass(this.variable);
-        } else if (this.isVariable(this.variable)){
-            console.log(this.isVariable(this.variable, 'String'));
-            this.goVariable(this.variable);
+        } else if (this.isVariable(this.variable, ['int', 'String', 'long', 'boolean'])){
+            this.goVariable(this.variable, ['int', 'String', 'long', 'boolean']);
         } else {
-            console.log(this.isVariable(this.variable, 'String'));
+            console.log(this.getDirectory(this.url), this.variable, this.filetype);
+            this.goSource(this.getDirectory(this.url), this.variable, this.filetype);
         }
-
-        //window.open(get_baseUrl() + variable +  get_langType(), '_blank');
     }
 
     getVariable(variable){
         return variable.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
     }
+
     getFile(url) {
         var index = url.lastIndexOf('/') + 1;
         var filenameWithExtension = url.substr(index);
         return filenameWithExtension.split('.')[0]; // <-- added this line
     }
 
+    getDirectory(url){
+        return url.substring(0, url.lastIndexOf('/')) + "/";
+    }
 
     isFile (variable, url){
         return variable === this.getFile(url);
@@ -56,7 +59,8 @@ class Java {
     isVariable (variable, datatype){
         var value = false;
         $('span').each(function () {
-            if ($(this).text() === datatype) {
+            if (($(this).text() === datatype[0]) || $(this).text() === datatype[1]
+                || $(this).text() === datatype[2] || $(this).text() === datatype[3]) {
                 var td = $(this).parent().closest('td');
                 var spanValue = td.children('span').next('span').text();
                 if (spanValue.includes(variable)) {
@@ -90,11 +94,12 @@ class Java {
         });
     }
 
-    goVariable(variable){
+    goVariable(variable, datatype){
         $('span').each(function(){
-            if($(this).text() === "int"){
+            if (($(this).text() === datatype[0]) || $(this).text() === datatype[1]
+                || $(this).text() === datatype[2] || $(this).text() === datatype[3]) {
                 var td = $(this).parent().closest('td');
-                td.children('span').next('span').css("border", "solid 2px red");
+                td.children('span').next('span').delay(500).fadeIn(1100);
                 var spanValue = td.children('span').next('span').text();
                 if(spanValue.includes(variable)){
                     $(window).scrollTop($(this).offset().top);
@@ -102,5 +107,9 @@ class Java {
                 }
             }
         });
+    }
+
+    goSource(url, variable, filetype){
+        window.open(url + variable +  filetype, '_blank');
     }
 }
